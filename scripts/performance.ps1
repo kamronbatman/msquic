f4e63d1a0ee1ec8ce661e475e3d61d2554db3eb5
Thad House <thhous@microsoft.com>
2020-10-06 09:52:41 -0700

    Another build fix

--- scripts/performance.ps1
+++ scripts/performance.ps1
@@ -182,11 +182,11 @@ if ($Local) {
             $Session2 = New-PSSession -ComputerName $ComputerName -Credential $WinRMUser -ConfigurationName PowerShell.7
         } else {
             $Session = New-PSSession -ComputerName $ComputerName -ConfigurationName PowerShell.7
-            $Session2 = New-PSSession -ComputerName $ComputerName -Credential $WinRMUser -ConfigurationName PowerShell.7
+            Session2 = New-PSSession -ComputerName $ComputerName -ConfigurationName PowerShell.7
         }
     } else {
         $Session = New-PSSession -HostName "$Remote"
-        $Session2 = New-PSSession -ComputerName $ComputerName -Credential $WinRMUser -ConfigurationName PowerShell.7
+        $Session2 = New-PSSession -HostName "$Remote"
     }
 
     $RemoteAddress = $Session.ComputerName
